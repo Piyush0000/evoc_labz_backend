65e59dbc574ded31cb1b1e4f78379d50c5c46470
Piyush <rathorepiyush0000@gmail.com>
2026-01-12 19:56:43 +0530

    Add delete

--- temp_server.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();  
-const express = require('express');  
-const cors = require('cors');  
-const bookDemoRoutes = require('./routes/bookDemo');  
-  
-const app = express();  
